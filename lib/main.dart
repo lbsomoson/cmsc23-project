b33e15f879501d1569b8e2fe9c52ddc66f23936c
Lea Marie Somoson <lbsomoson1@up.edu.ph>
2024-05-13 23:18:27 +0800

    fix: resolve inports

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:project/screens/admin/admin_view_orgs.dart';
 import 'package:project/screens/donor/donor_dashboard.dart';
 import 'package:project/screens/donor/donor_profile.dart';
 import 'package:project/screens/donor/donor_signup.dart';
-import 'package:project/screens/donor_signin.dart';
+import 'package:project/screens/signin.dart';
 import 'package:project/screens/organization/org_dashboard.dart';
 import 'package:project/screens/organization/org_donation.dart';
 import 'package:project/screens/organization/org_drives.dart';
