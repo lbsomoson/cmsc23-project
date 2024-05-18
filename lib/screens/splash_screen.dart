5ea5a9aacb78b5c252c284962a6c3832d883c130
Lea Marie Somoson <lbsomoson1@up.edu.ph>
2024-05-18 17:45:21 +0800

    style: add animated gradient to splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,34 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:project/screens/signin.dart';
+import 'package:animate_gradient/animate_gradient.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  bool isChanged = false;
+
+  Color primaryColor = const Color.fromARGB(255, 51, 188, 83);
+  Color secondaryColor = const Color.fromARGB(255, 51, 188, 83);
+
+  List<Color> generatePrimaryColors(Color baseColor) {
+    return [
+      baseColor,
+      baseColor.withOpacity(0.8),
+      baseColor.withOpacity(0.6),
+    ];
+  }
+
+  List<Color> generateSecondaryColors(Color baseColor) {
+    return [
+      baseColor.withOpacity(0.7),
+      baseColor.withOpacity(0.4),
+      baseColor,
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: content(),
-    );
-  }
-
-  Widget content() {
-    return Center(
-      child: SizedBox(
-        child: AnimatedSplashScreen(
-          curve: accelerateEasing,
-          splash: 'assets/images/splash.gif',
-          nextScreen: const SignInScreen(),
-          splashTransition: SplashTransition.fadeTransition,
-          pageTransitionType: PageTransitionType.fade,
-          splashIconSize: 500,
-          duration: 4000,
-          backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned.fill(
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                child: AnimateGradient(
+                  primaryBeginGeometry: const AlignmentDirectional(0, 1),
+                  primaryEndGeometry: const AlignmentDirectional(0, 2),
+                  secondaryBeginGeometry: const AlignmentDirectional(2, 0),
+                  secondaryEndGeometry: const AlignmentDirectional(0, -0.8),
+                  textDirectionForGeometry: TextDirection.ltr,
+                  primaryColors: generatePrimaryColors(primaryColor),
+                  secondaryColors: generateSecondaryColors(secondaryColor),
+                ),
+              ),
+            ),
+            SizedBox(
+              child: AnimatedSplashScreen(
+                curve: Curves.easeInOut,
+                splash: 'assets/images/splash.gif',
+                nextScreen: const SignInScreen(),
+                splashTransition: SplashTransition.fadeTransition,
+                pageTransitionType: PageTransitionType.fade,
+                splashIconSize: 500,
+                duration: 4000,
+                backgroundColor: Colors.transparent,
+              ),
+            ),
+          ],
         ),
       ),
     );
