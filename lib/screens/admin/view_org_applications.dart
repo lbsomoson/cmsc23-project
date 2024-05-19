5a00dbb970b9f35eb291cbdad189babbc449d160
Lea Marie Somoson <lbsomoson1@up.edu.ph>
2024-05-19 19:17:48 +0800

    feat: design and layout admin view all org applications

---
+++ lib/screens/admin/view_org_applications.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:project/widgets/appbar_title.dart';
+import 'package:project/widgets/org_application_card.dart';
+
+class ViewOrgApplications extends StatefulWidget {
+  const ViewOrgApplications({super.key});
+
+  @override
+  State<ViewOrgApplications> createState() => _ViewOrgApplicationsState();
+}
+
+class _ViewOrgApplicationsState extends State<ViewOrgApplications> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(title: const AppBarTitle(title: "Organization Applications")),
+      body: SafeArea(
+        child: GridView.builder(
+          padding: const EdgeInsets.all(8.0),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 8.0,
+            crossAxisSpacing: 8.0,
+            childAspectRatio: 0.62,
+          ),
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return const OrgApplicationCard();
+          },
+        ),
+      ),
+    );
+  }
+}
