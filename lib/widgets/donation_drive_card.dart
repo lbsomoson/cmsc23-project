61c744a421d1b3c694c0d70cf2a1c7abdcc2cf3f
Lea Marie Somoson <lbsomoson1@up.edu.ph>
2024-05-29 18:52:00 +0800

    chore: start org drives card for admin view

---
+++ lib/widgets/donation_drive_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DonationDriveCard extends StatefulWidget {
+  const DonationDriveCard({super.key});
+
+  @override
+  State<DonationDriveCard> createState() => _DonationDriveCardState();
+}
+
+class _DonationDriveCardState extends State<DonationDriveCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
