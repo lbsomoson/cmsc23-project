0ef2830f1a9c22b78895011769c195760e9f45dd
PsymonSezArcedera <pearcedera@up.edu.ph>
2024-05-31 10:24:37 +0800

    feat: Added qr code scanner

--- lib/widgets/qrscanner.dart
+++ lib/widgets/qrscanner.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_bar_code_scanner_dialog/qr_bar_code_scanner_dialog.dart';
 
+class QrScanner extends StatefulWidget {
+  const QrScanner({super.key});
+
+  @override
+  State<QrScanner> createState() => _QrScannerState();
+}
+
+class _QrScannerState extends State<QrScanner> {
+  final _qrBarCodeScannerDialogPlugin = QrBarCodeScannerDialog();
+  String? code;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          _qrBarCodeScannerDialogPlugin.getScannedQrBarCode(
+              context: context,
+              onCode: (code) {
+                setState(() {
+                  this.code = code;
+                });
+              });
+        },
+        child: Text(code ?? "Click me"),
+      ),
+    );
+  }
+}
