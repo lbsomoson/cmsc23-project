4de58b0b9c76be60007e469218bed58910ac43a3
Heather Bedonia <heatherbedonia@Heathers-Laptop.local>
2024-05-31 12:54:58 +0800

    feat: added changes to toggle switch

--- lib/main.dart
+++ lib/main.dart
@@ -111,7 +111,7 @@ class _RootWidgetState extends State<RootWidget> {
           ),
         ),
       ),
-      initialRoute: "/login",
+      initialRoute: "/donor-dashboard",
       onGenerateRoute: (settings) {
         if (settings.name == "/") {
           return MaterialPageRoute(builder: (context) => const SplashScreen());
--- lib/screens/donor/donor_donation_form.dart
+++ lib/screens/donor/donor_donation_form.dart
@@ -7,6 +7,7 @@ import 'package:project/widgets/text2.dart';
 import 'package:project/widgets/textfield.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
+import '../../widgets/datepicker.dart';
 import '../../widgets/image_upload2.dart';
 
 class DonorDonationForm extends StatefulWidget {
@@ -19,6 +20,7 @@ class DonorDonationForm extends StatefulWidget {
 class _DonorDonationFormState extends State<DonorDonationForm> {
   final _formKey = GlobalKey<FormState>();
   List<String> _selectedTypes = [];
+  bool isPickUp = true;
 
   late Map<String, dynamic> addDonation = {
     'types': [],
@@ -35,6 +37,15 @@ class _DonorDonationFormState extends State<DonorDonationForm> {
         padding: const EdgeInsets.only(bottom: 30),
         child: Text(
           errorMessage!,
+          style: const TextStyle(
+              color: Colors.red, fontSize: 14, fontWeight: FontWeight.w400),
+        ),
+      );
+
+  Widget get noDateMessage => Padding(
+        padding: const EdgeInsets.only(bottom: 30),
+        child: Text(
+          errorMessage2!,
           style: const TextStyle(
               color: Colors.red, fontSize: 14, fontWeight: FontWeight.w400),
         ),
@@ -115,7 +126,7 @@ class _DonorDonationFormState extends State<DonorDonationForm> {
                     CheckboxListTile(
                       controlAffinity: ListTileControlAffinity.leading,
                       title: Text2Widget(
-                      text: 'Necessities', style: 'labelMedium'),
+                          text: 'Necessities', style: 'labelMedium'),
                       value: _selectedTypes.contains('Necessities'),
                       onChanged: (checked) {
                         setState(() {
@@ -151,76 +162,101 @@ class _DonorDonationFormState extends State<DonorDonationForm> {
                       label: "Weight of items to donate in kg/lbs",
                       isRequired: true,
                     ),
-                  const Text2Widget(
-                      text: "Upload Image", style: "sectionHeader"),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  ImageUpload2Widget(
-                    callBack: (String path, File file) => {
-                      addDonation['file'] = file,
-                      addDonation['path'] = path,
-                      setState(() {
-                        showImageUploadMessage = false;
-                      })
-                    },
-                  ),
-                  showImageUploadMessage
-                      ? Column(
-                          children: [
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            imageUploadMessage,
-                          ],
-                        )
-                      : Container(),
-                  const SizedBox(
-                    height: 20,
-                  ),
-
-                  DividerWidget(),
-
-                  SizedBox(
-      height: 50,
-      child: ToggleSwitch(
-        minWidth: 250.0,
-        cornerRadius: 20.0,
-        activeBgColors: [const [Color.fromRGBO(62, 218, 134, 1)],const [Color.fromRGBO(62, 218, 134, 1)]],
-        activeFgColor: Colors.white,
-        inactiveBgColor: Colors.grey,
-        inactiveFgColor: Colors.white,
-        initialLabelIndex: 1,
-        totalSwitches: 2,
-        labels: ['Pick Up', 'Drop Off'],
-        radiusStyle: true,
-        onToggle: (index) {
-          print('switched to: $index');
-        },
-      ),
-    ),
-
-
-
-
-
-
-
-
-                    TextFieldWidget(
-                      callback: (String val) => addDonation['address'] = val,
-                      hintText: "Address",
-                      type: "String",
-                      label: "Address",
+                    const Text2Widget(
+                        text: "Upload Image", style: "sectionHeader"),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ImageUpload2Widget(
+                      callBack: (String path, File file) => {
+                        addDonation['file'] = file,
+                        addDonation['path'] = path,
+                        setState(() {
+                          showImageUploadMessage = false;
+                        })
+                      },
+                    ),
+                    showImageUploadMessage
+                        ? Column(
+                            children: [
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              imageUploadMessage,
+                            ],
+                          )
+                        : Container(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    DividerWidget(),
+                    DatePickerWidget(
+                      callback: (String val) => {
+                        addDonation['date'] = val,
+                        setState(() {
+                          showDateMessage = false;
+                        })
+                      },
+                      hintText: "Date of Pickup/Dropoff",
                       isRequired: true,
-                    ),   
-                    TextFieldWidget(
-                      callback: (String val) => addDonation['contactNumber'] = val,
-                      hintText: "Contact Number",
-                      type: "Number",
-                      label: "Contact Number",
-                      isRequired: true,
-                    ),  
+                      label: "Date of Pickup/Dropoff",
+                    ),
+                    showDateMessage
+                        ? Column(
+                            children: [
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              noDateMessage,
+                            ],
+                          )
+                        : Container(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text2Widget(
+                        text: "Choose Mode of Transfer",
+                        style: "sectionHeader"),
+                    SizedBox(
+                      height: 50,
+                      child: ToggleSwitch(
+                        minWidth: 250.0,
+                        cornerRadius: 20.0,
+                        activeBgColors: [
+                          const [Color.fromRGBO(62, 218, 134, 1)],
+                          const [Color.fromRGBO(62, 218, 134, 1)]
+                        ],
+                        activeFgColor: Colors.white,
+                        inactiveBgColor: Colors.grey,
+                        inactiveFgColor: Colors.white,
+                        initialLabelIndex: isPickUp ? 0 : 1,
+                        totalSwitches: 2,
+                        labels: ['Pick Up', 'Drop Off'],
+                        radiusStyle: true,
+                        onToggle: (index) {
+                          setState(() {
+                            isPickUp = index == 0;
+                          });
+                        },
+                      ),
+                    ),
+                    if (isPickUp) ...[
+                      TextFieldWidget(
+                        callback: (String val) => addDonation['address'] = val,
+                        hintText: "Address",
+                        type: "String",
+                        label: "Address",
+                        isRequired: true,
+                      ),
+                      TextFieldWidget(
+                        callback: (String val) =>
+                            addDonation['contactNumber'] = val,
+                        hintText: "Contact Number",
+                        type: "Number",
+                        label: "Contact Number",
+                        isRequired: true,
+                      ),
+                    ],
                   ],
                 ),
               ),
